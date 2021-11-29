5fcf499aeab450ff220e89a9b0ff8e660594105b
iyloteambwp <94965523+iyloteambwp@users.noreply.github.com>
2021-11-29 10:17:11 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,15 +19,6 @@
 		var $window = $(window),
 			$body = $('body');
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
-
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 70);
-			});
-
 		// Fix: Placeholder polyfill.
 			$('form').placeholder();
 
